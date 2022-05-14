d6a4064c69980d952ad992db483eab1776aea39f
hp <hp@LAPTOP-RH8KN3BE>
2022-05-14 18:33:57 +0530

    basicException in Java

---
+++ P14_Q1/src/basicException.java
@@ -0,0 +1,15 @@
+public class basicException 
+{
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("Without use try and catch");
+		int a = 100;
+		int b = 0;
+		int c = a/b;
+		System.out.println("Done without using try and catch");
+
+	}
+
+}
