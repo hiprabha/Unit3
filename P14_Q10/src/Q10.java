4324f5ecf1153a6b698fb571ff43c7718b11a62f
hp <hp@LAPTOP-RH8KN3BE>
2022-05-14 19:32:42 +0530

    checkedAndUncheckedException in Java

---
+++ P14_Q10/src/Q10.java
@@ -0,0 +1,16 @@
+class Q10
+{
+	public static void main(String[] args) {
+		 
+		 /* checked vs unchecked exception example */
+		 try {
+		  Class.forName("com.mcnz.Example");
+		 } catch (ClassNotFoundException e) {
+		  System.out.println("Class was not found.");
+		 }
+		    
+		 String input = null;
+		 input.length(); // throws an unchecked exception
+		    
+		}
+}
